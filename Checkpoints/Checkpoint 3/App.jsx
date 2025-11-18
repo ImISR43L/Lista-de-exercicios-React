b4eb7a7a2a5a6deb8754909ba4c3613d53b3560a
Israel <raeell26@gmail.com>
2025-11-18 10:46:10 -0300

    Savepoint-2

--- Checkpoints/Checkpoint 3/App.jsx
+++ Checkpoints/Checkpoint 3/App.jsx
@@ -2,22 +2,43 @@ import { useState } from "react";
 import "./App.css";
 
 function Header() {
+  const [musica, setMusica] = useState("");
   return (
-    <div class="flex-container">
-      <div style={{ width: "200", height: "200" }}>
-        <img
-          src="src\assets\spotify_logo.png"
-          alt="spotify_logo"
-          width="32px"
-          height="32px"
-        />
+    <div class="flex-container header-spotify">
+      <div style={{ width: "200", height: "200" }} class="logo-spotify">
+        <button id="button-logo-spotify">
+          <img
+            src="src\assets\spotify_logo.png"
+            alt="spotify_logo"
+            width="32px"
+            height="32px"
+          />
+        </button>
       </div>
-      <div style={{ width: "300px", margin: "0 400px" }}>
-        <button>Voltar</button>
-        <input type="text" name="" id="" />
+
+      <div style={{ width: "300px" }} class="flex-container">
+        <div>
+          <button id="botao-voltar-header">
+            <img
+              src="src\assets\home-page-white.png"
+              alt="home-page-logo"
+              width="24px"
+              height="24px"
+            />
+          </button>
+        </div>
+        <div>
+          <input
+            type="text"
+            value={musica}
+            onChange={(e) => setMusica(e.target.value)}
+          />
+        </div>
       </div>
-      <div>
-        Inscrever-se <button>Entrar</button>
+
+      <div class="flex-container auth-button">
+        <div style={{ width: "100px" }}>Inscrever-se</div>
+        <button>Entrar</button>
       </div>
     </div>
   );
